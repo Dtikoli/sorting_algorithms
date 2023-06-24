8724869c6645c5dd3aed325a05d692fb734724fb
Dtikoli <tikolidavid@yahoo.com>
2023-06-24 02:19:55 -0700

    Insertion algorithm

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,56 @@
+#include "sort.h"
+
+/**
+ * node_swap - swap elements in an list
+ * @head: head of list
+ * @node1: first node
+ * @node2: second node
+ */
+void node_swap(listint_t **head, listint_t *node1, listint_t *node2)
+{
+	listint_t *tmp1 = NULL, *tmp2 = NULL;
+
+	if (node1 == NULL || node2 == NULL)
+		return;
+	tmp1 = node1->prev;
+	tmp2 = node2->next;
+	if (tmp1) /* if nodes are adjacent */
+		tmp1->next = node2;
+	if (tmp2)
+		tmp2->prev = node1;
+	node1->next = tmp2;
+	node1->prev = node2;
+	node2->next = node1;
+	node2->prev = tmp1;
+	if (tmp1 == NULL)
+		*head = node2;
+}
+
+/**
+ * insertion_sort_list  - insertion_sort_list
+ * @list: double point to the head of a doubly liked list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *prev;
+	int val;
+
+	if (list == NULL || (*list)->next == NULL || (*list) == NULL)
+	{
+		return;
+	}
+	current = *list;
+	while (current)
+	{
+		prev = current->prev;
+				val = current->n;
+
+		while (prev && prev->n > val)
+		{
+			node_swap(list, prev, current);
+			print_list(*list);
+			prev = current->prev;
+		}
+		current = current->next;
+	}
+}
